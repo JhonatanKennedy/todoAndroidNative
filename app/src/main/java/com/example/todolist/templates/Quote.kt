bcb4e233d67aa7fd2c36413b671d6a5ea25bc97f
Jhonatan Kennedy <jhonatankennedy@live.com>
2025-04-08 19:16:24 -0300

    Add: api reference

--- app/src/main/java/com/example/todolist/templates/Quote.kt
+++ app/src/main/java/com/example/todolist/templates/Quote.kt
@@ -1,5 +1,8 @@
 package com.example.todolist.templates
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,6 +26,7 @@ import com.example.todolist.viewModel.QuoteViewModel
 @Composable
 fun QuoteTemplate(viewModel: QuoteViewModel = viewModel ()){
     val quote by viewModel.quote.collectAsState()
+    val context = LocalContext.current
 
     Scaffold { paddingValues ->
         Column (
@@ -31,6 +36,19 @@ fun QuoteTemplate(viewModel: QuoteViewModel = viewModel ()){
             Title("Quote of the day")
             Spacer(modifier = Modifier.height(8.dp))
             Text(quote, style = MaterialTheme.typography.bodyLarge, textAlign = TextAlign.Center)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "See api here",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.clickable {
+                    val url = "https://zenquotes.io/"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    context.startActivity(intent)
+                }
+            )
         }
     }
 }
